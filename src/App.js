e0f15e0c36bf2d8f5c2895f6bd38c3ea47e00689
Ian <ian@g.ucla.edu>
2024-02-11 13:21:25 -0800

    aws deployment retry-1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Code Here.</p>
+        <p>Code Here. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
