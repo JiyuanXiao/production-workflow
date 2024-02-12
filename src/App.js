83b769a66394e422becdab4de401995f28857c73
Ian <ian@g.ucla.edu>
2024-02-11 21:38:13 -0800

    made a change in feature branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Code Here. </p>
+        <p>Feature Branch Change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
